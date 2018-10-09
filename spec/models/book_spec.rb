35de82900ba8837d8847e7a9a8baf3a030dedf76
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-09 17:17:00 -0600

    validation tests for book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 describe Book, type: :model do
 
   describe 'Validations' do
-    
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :pages}
+    it { should validate_presence_of :year}
   end
 
 end
