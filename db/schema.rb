5d5d86a0cb530f928b179f1d2710e7d5eed749d8
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-10 14:31:28 -0600

    Review model updated with relation to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20181010180333) do
+ActiveRecord::Schema.define(version: 20181010182348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,14 +34,6 @@ ActiveRecord::Schema.define(version: 20181010180333) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "book_authors", "authors"
-  add_foreign_key "book_authors", "books"
-
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-  end 
-  
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -53,6 +44,11 @@ ActiveRecord::Schema.define(version: 20181010180333) do
     t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
-  add_foreign_key "reviews", "books"
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+  end
 
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
+  add_foreign_key "reviews", "books"
 end
