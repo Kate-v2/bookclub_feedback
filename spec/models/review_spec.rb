891b81dfa582194eaca3695d46e0d6d8fa7f6bc3
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-10 11:57:59 -0600

    review creation through book tested

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -15,4 +15,20 @@ describe Review, type: :model do
     it { should belong_to :book }
   end
 
+  describe 'Creation' do
+
+    it 'should create a review through a book' do
+      book = Book.create(title: "Title 1", pages: 100,  year: 2000 )
+      book.reviews.create(title: "Review 1", description: "description 1", score: 3)
+      review = book.reviews.first
+      expect(review.title).to eq("Review 1")
+      expect(review.description).to eq("description 1")
+      expect(review.score).to eq(3)
+      expect(book.reviews.count).to eq(1)
+    end
+
+
+
+  end
+
 end
