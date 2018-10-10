d9068e89a7e6d3d7ccc9e673a3b7853fea4c0e1a
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-10 12:29:58 -0600

    create user model and migration (with tests)

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009232403) do
-
+ActiveRecord::Schema.define(version: 20181010182348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "authors", force: :cascade do |t|
     t.string "name"
-  end 
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -27,7 +25,10 @@ ActiveRecord::Schema.define(version: 20181009232403) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
