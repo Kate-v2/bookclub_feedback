257609011f725cd96bd09420aa5228c23d483ebc
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-10 14:51:52 -0600

    thought these were submitted

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -19,8 +19,9 @@ describe Review, type: :model do
   describe 'Creation' do
 
     it 'should create a review through a book' do
-      book = Book.create(title: "Title 1", pages: 100,  year: 2000 )
-      book.reviews.create(title: "Review 1", description: "description 1", score: 3)
+      user1 = User.create(name: "User 1")
+      book  = Book.create(title: "Title 1", pages: 100,  year: 2000 )
+      book.reviews.create(title: "Review 1", description: "description 1", score: 3, user_id: user1.id)
       review = book.reviews.first
       expect(review.title).to eq("Review 1")
       expect(review.description).to eq("description 1")
@@ -29,14 +30,24 @@ describe Review, type: :model do
     end
 
     it 'should create multiple reviews through a book' do
-      book = Book.create(title: "Title 1", pages: 100,  year: 2000 )
-      book.reviews.create(title: "Review 1", description: "description 1", score: 3)
-      book.reviews.create(title: "Review 2", description: "description 2", score: 2)
+      user1 = User.create(name: "User 1")
+      book  = Book.create(title: "Title 1", pages: 100,  year: 2000 )
+      book.reviews.create!(title: "Review 1", description: "description 1", score: 3, user_id: user1.id)
+      book.reviews.create!(title: "Review 2", description: "description 2", score: 2, user_id: user1.id)
       review1 = book.reviews.first
       review2 = book.reviews.last
       expect(review1.title).to eq("Review 1")
       expect(review2.title).to eq("Review 2")
       expect(book.reviews.count).to eq(2)
+    end
+
+    it 'should create a review through a user' do
+      user1 = User.create(name: "User 1")
+      book  = Book.create(title: "Title 1", pages: 100,  year: 2000 )
+      user1.reviews.create(title: "Review 1", description: "description 1", score: 3, book_id: book.id)
+      review = user1.reviews.first
+      expect(user1.reviews.count).to eq(1)
+      expect(review.title).to eq("Review 1")
     end
 
 
