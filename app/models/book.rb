bf8cbb8d5e1b2681cad2b3df344a5848f2a62a80
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-11 10:59:49 -0600

    code spike, experimenting with structure of making a new book -- needs tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,6 +9,49 @@ class Book < ApplicationRecord
   has_many :authors, through: :book_authors
 
 
+  # --- Creation ---
+
+  def make_new_book(params)
+    title = assess_title(params[:title])
+    return title if title == duplicate_error
+    book = Book.create(title: title, pages: params[:pages], year: params[:year])
+    authors = assess_authors(params[:authors])
+    # collection = authors.map { |name| find_an_author(name) }
+    authors.each { |name| find_and_add_author(name, book) }
+  end
+
+  def assess_authors(csv)
+    return [csv] if !csv.include?(',')
+    csv.include?(", ") ? csv.split(", ") : csv.include?(",")
+  end
+
+  def find_and_add_author(name, book)
+    name = name.titleize
+    author = Author.find_by_name(name)
+    author ? pair_with_author(author, book) : book.authors.create(name: name)
+  end
+
+  def pair_with_author(author, book)
+    book.authors << author
+    # IS THIS REDUNDANT ??
+    author.books << book
+  end
+
+  def assess_title(title)
+    title = title.titleize
+    invalid = Book.find_by_title(title)
+    invalid ? duplicate_error : title
+  end
+
+  def duplicate_error
+    "ERROR - entry already exists."
+  end
+
+
+
+
+
+
   # --- Math ---
 
   def count_ratings
@@ -27,13 +70,14 @@ class Book < ApplicationRecord
     order(:title)
   end
 
-  # This doesn't quite work yet 
+  # This doesn't quite work yet
   def self.sort_by_average_rating
     books = Book.all
     books.select(:books, :'average_rating AS av_score')
     .joins(:results)
     .order(:av_score)
   end
+
 
 
 
