124934d244386bbd78766b82b2cec1a273a6f35f
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-13 10:32:41 -0600

    WIP

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,6 +5,7 @@ describe Book, type: :model do
 
   describe 'Validations' do
     it { should validate_presence_of :title}
+    it { should validate_uniqueness_of :title}
     it { should validate_presence_of :pages}
     it { should validate_presence_of :year}
   end
