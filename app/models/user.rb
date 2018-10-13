9dd2b37491877a6c52172faf2d49c6bd50bee527
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-13 13:22:43 -0600

    finished users page with oldest and newest sort links as well as testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,21 @@ class User < ApplicationRecord
 
   has_many :reviews
 
+  def assess_params(params)
+    if params[:sort] == 'oldest'
+      sort_by_oldest
+    elsif params[:sort] == 'newest'
+      sort_by_newest
+    else 
+      reviews.all
+    end
+  end
 
+  def sort_by_oldest
+    reviews.order("created_at ASC")
+  end
+
+  def sort_by_newest
+    reviews.order("created_at DESC")
+  end
 end
